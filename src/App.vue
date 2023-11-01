313318feacf3876675e97c0cfd4d986fe51687fa
Roman Tonkoshkurov <44974582+Renhor@users.noreply.github.com>
2023-11-01 17:22:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import TestUnionProps from './components/TestUnionProps.vue';
+</script>
+
+<template>
+  <div>
+    <TestUnionProps :my-prop="'autocomplete'" />
+
+    <TestUnionProps my-prop="is" />
+
+    <TestUnionProps my-prop="" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/TestUnionProps.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+interface Props {
+  myProp?: 'autocomplete' | 'with' | 'union' | 'props' | 'is' | 'not' | 'works'
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <!--  -->
+  </div>
+</template>
+
+<style lang="scss" module>
+//
+</style>
